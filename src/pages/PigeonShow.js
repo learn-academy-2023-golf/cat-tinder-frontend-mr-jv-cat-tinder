44fd23739523e6f6b8239c33a9253952e049daf0
LEARN Academy <MichaelDReyes5@gmail.com>
2023-12-08 14:28:00 -0800

    fixed indentation and removed unused code

--- src/pages/PigeonShow.js
+++ src/pages/PigeonShow.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 const PigeonShow = ({pigeons}) => {
   const {id} = useParams()
-    // console.log(id)
+    
   let selectedPigeon = pigeons.find(pigeonObject => pigeonObject.id === +id)
   console.log(selectedPigeon)
   
